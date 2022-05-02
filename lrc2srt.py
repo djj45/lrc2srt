79a58e0374250a5e5bc199dca13bf082a0bab1d5
djj45 <ssjj_djj45@163.com>
2022-05-02 23:36:17 +0800

    忘记改版本了

--- lrc2srt.py
+++ lrc2srt.py
@@ -7,7 +7,7 @@ INTERVAL = 8
 headers = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36"
 }
-version = 20220502.1427
+version = 20220502.1913
 
 
 def check_version():
